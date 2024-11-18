1821bf1db87bb279809a0c27688cc4e74cca2e75
ORACLEO-SVKJCNH\Ashish Rai <ashish.a.rai@oracle.com>
2024-11-18 19:12:36 +0530

    Initial Commit

---
+++ restful-web-services/src/main/java/com/ashishrai/rest/webservices/user/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.ashishrai.rest.webservices.user;
+
+public class UserNotFoundException extends RuntimeException {
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+}
