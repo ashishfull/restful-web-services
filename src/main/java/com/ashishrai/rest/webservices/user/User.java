3532d0d6e86886a591a3042aa6a7f7a8f6859ac6
ORACLEO-SVKJCNH\Ashish Rai <ashish.a.rai@oracle.com>
2024-11-21 17:49:48 +0530

    changes for h2

--- src/main/java/com/ashishrai/rest/webservices/user/User.java
+++ src/main/java/com/ashishrai/rest/webservices/user/User.java
@@ -1,12 +1,19 @@
+
 package com.ashishrai.rest.webservices.user;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
+@Entity(name = "user_details")
 public class User {
 
+	@Id
+	@GeneratedValue
 	private Integer id;
 	@Size(min = 2, message = "User's name should be at least 2 characters long")
 	private String name;
@@ -14,6 +21,7 @@ public class User {
 	private LocalDate birthDate;
 
 	public User(Integer id, String name, LocalDate birthDate) {
+
 		super();
 		this.id = id;
 		this.name = name;
@@ -21,32 +29,38 @@ public class User {
 	}
 
 	public Integer getId() {
+
 		return id;
 	}
 
 	public void setId(Integer id) {
+
 		this.id = id;
 	}
 
 	public String getName() {
+
 		return name;
 	}
 
 	public void setName(String name) {
+
 		this.name = name;
 	}
 
 	public LocalDate getBirthDate() {
+
 		return birthDate;
 	}
 
 	public void setBirthDate(LocalDate birthDate) {
+
 		this.birthDate = birthDate;
 	}
 
 	@Override
 	public String toString() {
+
 		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
 	}
-
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into user_details(id, birth_date, name)
+values(10001, current_date(), 'Ashish');
+
+insert into user_details(id, birth_date, name)
+values(10002, current_date(), 'Anjali');
+
+insert into user_details(id, birth_date, name)
+values(10003, current_date(), 'Radha');
+
+insert into user_details(id, birth_date, name)
+values(10004, current_date(), 'Krishna');
